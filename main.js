d80f3d8d9072519a11f1531cb85489eefed31ee0
daysantace <daysant@proton.me>
2024-05-10 18:25:40 +0100

    mobile

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 // Copyright (c) 2023 - 2024 daysant, Elysia MC - Elysia MC website
 // This file is licensed under the terms of the AGPL v3.0-or-later.
 // daysant@proton.me, discord.gg/RKAf97d5Ap
+
+var hamburger = false
+
 function setRandomBackground() {
     var img = Math.floor(Math.random()*12)+1;
     var prev_img = Math.floor(Math.random()*12)+1;
@@ -12,18 +15,35 @@ function setRandomBackground() {
     root.style.setProperty('--image', `url('${img}'`);
 }
 
+function section(section) {
+  var sections = document.getElementsByTagName('section');
+    for (var i = 0; i < sections.length; i++) {
+      sections[i].style.display = 'none';
+  }
+
+  var element = document.getElementById(section);
+  if (element) {
+    element.style.display = 'block';
+  }
+  
+  if (hamburger){
+    cookTheHamburger()
+  }
+}
+
+function cookTheHamburger() {
+  hamburger=!hamburger
+  
+  if (hamburger){
+    document.getElementById('hamburger').innerHTML = 'x'
+    document.querySelector('nav').style.display = 'flex';
+  } else {
+    document.getElementById('hamburger').innerHTML = '≡'
+    document.querySelector('nav').style.display = 'none';
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   setRandomBackground()
   section('home');
 });
-
-function section(section) {
-  document.querySelectorAll('section').forEach(section => {section.style.display = 'none';});
-
-  const element = document.getElementById(section);
-  if (element) {
-    element.style.display = 'block';
-  } else {
-    console.error(`element with id '${section}' not found.`);
-  }
-}
